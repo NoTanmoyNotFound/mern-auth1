da8f3858e1c4e512fbffb206b7eb4b3eb656252f
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-01-13 10:47:59 +0530

    setting up mailtrap

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,30 @@
+import {MailtrapClient} from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// const TOKEN = process.env.MAILTRAP_TOKEN ;
+
+export const mailtrapClient = new MailtrapClient({
+  token: process.env.MAILTRAP_TOKEN,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Tanmoy Das",
+};
+// const recipients = [
+//   {
+//     email: "tanmoydas78416@gmail.com",
+//   }
+// ];
+
+// client
+//   .send({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     text: "Congrats for sending test email with Mailtrap!",
+//     category: "Integration Test",
+//   })
+//   .then(console.log, console.error); 

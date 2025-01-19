802cc2e6f39e80d4a0646365e9c569d029832c10
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-01-19 12:22:33 +0530

    adding more in frontend

--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import {motion} from 'framer-motion'
-const floatingShape = ({color,size,top,left,delay}) => {
+const FloatingShape = ({color,size,top,left,delay}) => {
   return (
     <motion.div
-        className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl ${top} ${left}`}
+        className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl `}
+        style={{top,left}}
         animate={{
             y: ["0%", "100%", "0%"],
             x: ["0%", "100%", "0%"],
             rotate: [0,360],
         }}
-        
-    >
+        transition={{
+          duration: 20,
+          ease:"linear",
+          repeat: Infinity,
+          delay
+        }}
+        aria-hidden="true"
+    />
       
-    </motion.div>
+    
   )
 }
 
-export default floatingShape
+export default FloatingShape

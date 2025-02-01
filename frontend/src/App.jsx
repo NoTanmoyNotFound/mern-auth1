7e662c1e22da5b7584f87f3757b820cefa233690
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2025-02-01 14:14:09 +0530

    making routes for reset-pass

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import { useAuthStore } from "./store/authStore"
 import { useEffect } from "react"
 import DashboardPage from "./pages/DashboardPage"
 import LoadingSpinner from "./components/LoadingSpinner"
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
 
 //protect routes that require authentication
 const protectedRoute =({children}) => {
@@ -71,6 +72,9 @@ function App() {
       
       }/>
       <Route path="/verify-email" element={<EmailVerificationPage/>}/>
+      <Route path="/forgot-password" element={<RedirectAuthenticatedUser>
+        <ForgotPasswordPage/>
+      </RedirectAuthenticatedUser>}/>
     </Routes>
 
     <Toaster />
